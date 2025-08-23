a2415edf712e625fb2bcfcbb06a35dda4f52a9fd
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-08-23 19:07:33 -0300

    feat: add ICreateDisciplineUseCase interface

---
+++ Quizzando/UseCases/Disciplines/Create/ICreateDisciplineUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Quizzando.Communication.Requests.Disciplines;
+using Quizzando.Communication.Responses.Disciplines;
+
+namespace Quizzando.UseCases.Disciplines.Create
+{
+    public interface ICreateDisciplineUseCase
+    {
+        Task<DisciplineResponse> Execute(DisciplineRequest request);
+    }
+}
