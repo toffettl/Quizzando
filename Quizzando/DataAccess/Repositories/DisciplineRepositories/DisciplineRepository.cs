dc889ee61cfc1e8a2060aa947a584d119171f26e
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-08-23 19:05:25 -0300

    feat: add DisciplineRepository

---
+++ Quizzando/DataAccess/Repositories/DisciplineRepositories/DisciplineRepository.cs
@@ -0,0 +1,21 @@
+﻿using Quizzando.DataAccess.Repositories.DisciplineRepository;
+using Quizzando.Models;
+
+namespace Quizzando.DataAccess.Repositories.DisciplineRepositories
+{
+    public class DisciplineRepository : IDisciplineWriteOnlyRepository  
+    {
+        private readonly QuizzandoDbContext _dbContext;
+
+        public DisciplineRepository(
+            QuizzandoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add(Discipline discipline)
+        {
+            await _dbContext.Discipline.AddAsync(discipline);
+        }
+    }
+}
