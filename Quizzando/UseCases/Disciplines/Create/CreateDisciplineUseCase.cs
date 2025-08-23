9e29bd904867c030be0b0ab01f39ec3f45f8b456
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-08-23 19:07:58 -0300

    feat: add CreateDisciplineUseCase

---
+++ Quizzando/UseCases/Disciplines/Create/CreateDisciplineUseCase.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Quizzando.Communication.Requests.Disciplines;
+using Quizzando.Communication.Responses.Disciplines;
+using Quizzando.DataAccess.Repositories;
+using Quizzando.DataAccess.Repositories.DisciplineRepository;
+using Quizzando.Exception.ExceptionsBase;
+using Quizzando.Models;
+
+namespace Quizzando.UseCases.Disciplines.Create
+{
+    public class CreateDisciplineUseCase : ICreateDisciplineUseCase
+    {
+        private readonly IDisciplineWriteOnlyRepository _disciplineWriteOnlyRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CreateDisciplineUseCase(
+            IDisciplineWriteOnlyRepository disciplineWriteOnlyRepository,
+            IUnitOfWork unitOfWork,
+            IMapper mapper)
+        {
+            _disciplineWriteOnlyRepository = disciplineWriteOnlyRepository;
+            _unitOfWork = unitOfWork; 
+            _mapper = mapper;
+        }
+
+        public async Task<DisciplineResponse> Execute(DisciplineRequest request)
+        {
+            Validate(request);
+
+            var discipline = _mapper.Map<Discipline>(request);
+
+            discipline.CreatedAt = DateTime.UtcNow;
+            discipline.UpdatedAt = DateTime.UtcNow;
+
+            await _disciplineWriteOnlyRepository.Add(discipline);
+
+            await _unitOfWork.Commit();
+
+            return _mapper.Map<DisciplineResponse>(discipline);
+        }
+
+        private void Validate(DisciplineRequest request)
+        {
+            var result = new DisciplineValidator().Validate(request);
+
+            if (result.IsValid == false)
+            {
+                var erroMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+                throw new ErrorOnValidationException(erroMessages);
+            }
+        }
+    }
+}
