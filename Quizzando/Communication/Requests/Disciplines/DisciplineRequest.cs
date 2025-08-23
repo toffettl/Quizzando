4dfb0235ba91437516a3e8668100aff22dae9181
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-08-23 19:05:54 -0300

    feat: add DisciplineRequest

---
+++ Quizzando/Communication/Requests/Disciplines/DisciplineRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Quizzando.Communication.Requests.Disciplines
+{
+    public class DisciplineRequest
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
