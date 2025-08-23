5101fd16b7c2192769b8b521dd0277541bc0cc89
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-08-23 19:13:32 -0300

    feat: add DisciplineController

---
+++ Quizzando/Controllers/DisciplineController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quizzando.Communication.Requests.Disciplines;
+using Quizzando.Communication.Responses;
+using Quizzando.Communication.Responses.Disciplines;
+using Quizzando.UseCases.Disciplines.Create;
+
+namespace Quizzando.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DisciplineController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(DisciplineResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Create(
+            [FromServices] ICreateDisciplineUseCase useCase,
+            [FromBody] DisciplineRequest request)
+        {
+            var response = await useCase.Execute(request);
+
+            return Ok(response);
+        }
+    }
+}
