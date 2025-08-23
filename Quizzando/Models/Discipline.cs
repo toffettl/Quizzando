5964962787ee8d41077e56a081d94baa4a52f32d
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-08-23 19:02:33 -0300

    feat: add Discipline entity

---
+++ Quizzando/Models/Discipline.cs
@@ -0,0 +1,13 @@
+﻿namespace Quizzando.Models
+{
+    public class Discipline
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
