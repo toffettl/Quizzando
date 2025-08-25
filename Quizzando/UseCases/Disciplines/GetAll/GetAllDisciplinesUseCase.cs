64a931fc966d843f4c0e7949340f0a79812a3006
aluno <etecae023@hotmail.com>
2025-08-25 08:35:12 -0300

    feat: add GetAllDisciplinesUseCase

---
+++ Quizzando/UseCases/Disciplines/GetAll/GetAllDisciplinesUseCase.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Quizzando.Communication.Responses.Disciplines;
+using Quizzando.DataAccess.Repositories.DisciplineRepositories;
+
+namespace Quizzando.UseCases.Disciplines.GetAll
+{
+    public class GetAllDisciplinesUseCase : IGetAllDisciplinesUseCase
+    {
+        private readonly IDisciplineReadOnlyRepository _disciplineReadOnlyRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllDisciplinesUseCase(
+            IDisciplineReadOnlyRepository disciplineReadOnlyRepository,
+            IMapper mapper)
+        {
+            _disciplineReadOnlyRepository = disciplineReadOnlyRepository;
+            _mapper = mapper;
+        }
+        public async Task<DisciplineResponses> Execute()
+        {
+            var disciplines = await _disciplineReadOnlyRepository.GetAll();
+
+            return new DisciplineResponses
+            {
+                Disciplines = _mapper.Map<List<DisciplineResponse>>(disciplines)
+            };
+        }
+    }
+}
