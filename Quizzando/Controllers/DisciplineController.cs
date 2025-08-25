dd4dbd2fb363c96bcb1781573a7e8644c5d6e678
aluno <etecae023@hotmail.com>
2025-08-25 08:36:31 -0300

    feat: add GetAll function to DisciplineController

--- Quizzando/Controllers/DisciplineController.cs
+++ Quizzando/Controllers/DisciplineController.cs
@@ -3,6 +3,7 @@ using Quizzando.Communication.Requests.Disciplines;
 using Quizzando.Communication.Responses;
 using Quizzando.Communication.Responses.Disciplines;
 using Quizzando.UseCases.Disciplines.Create;
+using Quizzando.UseCases.Disciplines.GetAll;
 using Quizzando.UseCases.Disciplines.GetById;
 
 namespace Quizzando.Controllers
@@ -24,13 +25,29 @@ namespace Quizzando.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(typeof(DisciplineResponses), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> GetAll(
+            [FromServices] IGetAllDisciplinesUseCase useCase)
+        {
+            var response = await useCase.Execute();
+
+            if (response.Disciplines?.Count == 0)
+            {
+                return NoContent();
+            }
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
         [ProducesResponseType(typeof(DisciplineResponse), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetById(
-            [FromServices] GetDisciplineByIdUseCase useCase,
+            [FromServices] IGetDisciplineByIdUseCase useCase,
             [FromRoute] Guid id)
         {
-            var response = await  useCase.Execute(id);
+            var response = await useCase.Execute(id);
 
             return Ok(response);
         }
