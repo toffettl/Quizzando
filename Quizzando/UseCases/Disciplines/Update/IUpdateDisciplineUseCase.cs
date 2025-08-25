04bdc0e8eada194f5abb7946384002b0cb78ab2d
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-25 18:42:19 -0300

    feat: add IUpdateDisciplineUseCase interface

---
+++ Quizzando/UseCases/Disciplines/Update/IUpdateDisciplineUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Quizzando.Communication.Requests.Disciplines;
+
+namespace Quizzando.UseCases.Disciplines.Update
+{
+    public interface IUpdateDisciplineUseCase
+    {
+        Task Execute(Guid id, DisciplineRequest request);
+    }
+}
