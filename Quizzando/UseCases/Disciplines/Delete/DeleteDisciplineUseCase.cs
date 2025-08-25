19146288c311f5de19d6d719aa4318a32523270d
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-25 19:00:07 -0300

    feat: add DeleteDisciplineUseCase

---
+++ Quizzando/UseCases/Disciplines/Delete/DeleteDisciplineUseCase.cs
@@ -0,0 +1,32 @@
+﻿using Quizzando.DataAccess.Repositories;
+using Quizzando.DataAccess.Repositories.DisciplineRepository;
+using Quizzando.Exception;
+
+namespace Quizzando.UseCases.Disciplines.Delete
+{
+    public class DeleteDisciplineUseCase : IDeleteDisciplineUseCase
+    {
+        private readonly IDisciplineWriteOnlyRepository _disciplineWriteOnlyRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteDisciplineUseCase(
+            IDisciplineWriteOnlyRepository disciplineWriteOnlyRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _disciplineWriteOnlyRepository = disciplineWriteOnlyRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Execute(Guid id)
+        {
+            var result = await _disciplineWriteOnlyRepository.Delete(id);
+
+            if (result != true)
+            {
+                throw new DirectoryNotFoundException(ResourceErrorMessages.DISCIPLINE_NOT_FOUND);
+            }
+
+            await _unitOfWork.Commit();
+        }
+    }
+}
