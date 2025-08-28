2d3689609686afea6d1832685ec91ceb3b99b5c6
danielalmeidafr <d.almeidafrr@gmail.com>
2025-08-28 10:29:48 -0300

    alter context

--- Quizzando/DataAccess/QuizzandoDbContext.cs
+++ Quizzando/DataAccess/QuizzandoDbContext.cs
@@ -12,5 +12,8 @@ namespace Quizzando.DataAccess
 
         public DbSet<User> User {  get; set; }
         public DbSet<Question> Question { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Discipline> Discipline { get; set; }
+        public DbSet<Course> Course { get; set; }
     }
 }
