5489f5529a7b8101c69ab5950df83ecb48740776
aluno <etecae023@hotmail.com>
2025-09-03 10:12:34 -0300

    feat: add GetDisciplinesByCouseId

---
+++ Quizzando/UseCases/Disciplines/GetByCouseId/GetDisciplinesByCourseId.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Quizzando.Communication.Responses.Disciplines;
+using Quizzando.DataAccess.Repositories.DisciplineRepositories;
+
+namespace Quizzando.UseCases.Disciplines.GetByCouseId
+{
+    public class GetDisciplinesByCourseId : IGetDisciplinesByCouseId
+    {
+        private readonly IDisciplineReadOnlyRepository _disciplineReadOnlyRepository;
+        private readonly IMapper _mapper;
+
+        public GetDisciplinesByCourseId(
+            IDisciplineReadOnlyRepository disciplineReadOnlyRepository,
+            IMapper mapper)
+        {
+            _disciplineReadOnlyRepository = disciplineReadOnlyRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<DisciplineResponses> Execute(Guid courseId)
+        {
+            var disciplines = await _disciplineReadOnlyRepository.GetByCourseId(courseId);
+
+            return new DisciplineResponses
+            {
+                Disciplines = _mapper.Map<List<DisciplineResponse>>(disciplines)
+            };
+        }
+    }
+}
