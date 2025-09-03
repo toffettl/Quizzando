366425d5b9501a707703ab8242174c00ed0c37cc
aluno <etecae023@hotmail.com>
2025-09-03 10:12:20 -0300

    feat: add IGetDisciplineByCouseId interface

---
+++ Quizzando/UseCases/Disciplines/GetByCouseId/IGetDisciplinesByCouseId.cs
@@ -0,0 +1,9 @@
+﻿using Quizzando.Communication.Responses.Disciplines;
+
+namespace Quizzando.UseCases.Disciplines.GetByCouseId
+{
+    public interface IGetDisciplinesByCouseId
+    {
+        Task<DisciplineResponses> Execute(Guid courseId);
+    }
+}
