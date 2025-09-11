a42c7dcdd98120fff1c6a187346a2c45fbad95a6
toffettl <toffetti0824@gmail.com>
2025-09-11 07:59:45 -0300

    refactor: Change property names to PascalCase
    Changes the `Course_name` and `Discipline_id` properties to `CourseName` and `DisciplineId`, respectively, following the PascalCase naming convention.

--- Quizzando/Models/Course.cs
+++ Quizzando/Models/Course.cs
@@ -3,9 +3,9 @@ namespace Quizzando.Models
     public class Course
     {
         public Guid Id { get; set; }
-        public string? Course_name { get; set; }
+        public string? CourseName { get; set; }
 
-        public Guid Discipline_id { get; set; }
+        public Guid DisciplineId { get; set; }
         public ICollection<Discipline> Disciplines { get; set; } = new List<Discipline>();
     }
 }
