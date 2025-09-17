4ef765b5926fca3f242ea08f64912de5541aa565
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-09-17 10:35:31 -0300

    feat: add ICreateUserDisciplineUseCase

---
+++ Quizzando/UseCases/UserDisciplines/Create/ICreateUserDisciplineUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Quizzando.Communication.Requests.UserDiscipline;
+using Quizzando.Communication.Responses.UserDiscipline;
+
+namespace Quizzando.UseCases.UserDisciplines.Create
+{
+    public interface ICreateUserDisciplineUseCase
+    {
+        Task<UserDisciplineResponse> Execute(UserDisciplineRequest request);
+    }
+}
