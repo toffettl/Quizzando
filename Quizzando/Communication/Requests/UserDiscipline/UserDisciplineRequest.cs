5109ba725bb1228c9b8214df8f35c424720776fb
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-09-17 10:33:30 -0300

    feat: add UserDisciplineRequest

---
+++ Quizzando/Communication/Requests/UserDiscipline/UserDisciplineRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Quizzando.Communication.Requests.UserDiscipline
+{
+    public class UserDisciplineRequest
+    {
+        public long Time { get; set; }
+
+        public Guid UserId { get; set; }
+        public Guid DisciplineId { get; set; }
+    }
+}
