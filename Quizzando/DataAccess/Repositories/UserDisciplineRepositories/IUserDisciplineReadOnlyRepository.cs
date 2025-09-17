981c167a4b375d9aba1e9325215baa19d242fcab
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-09-17 10:32:33 -0300

    feat: add IUserDisciplineReadOnlyRepository interface

---
+++ Quizzando/DataAccess/Repositories/UserDisciplineRepositories/IUserDisciplineReadOnlyRepository.cs
@@ -0,0 +1,9 @@
+﻿using Quizzando.Models;
+
+namespace Quizzando.DataAccess.Repositories.UserDisciplineRepositories
+{
+    public interface IUserDisciplineReadOnlyRepository
+    {
+        Task<bool> ExistsWithUserIdAndDisciplineId(Guid userId, Guid disciplineId);
+    }
+}
