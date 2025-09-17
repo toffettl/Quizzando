be69eac08205617f67d4f3e61405d50da674070c
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-09-17 10:33:06 -0300

    feat: add UserDisciplineRepository

---
+++ Quizzando/DataAccess/Repositories/UserDisciplineRepositories/UserDisciplineRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Quizzando.Models;
+
+namespace Quizzando.DataAccess.Repositories.UserDisciplineRepositories
+{
+    public class UserDisciplineRepository : 
+        IUserDisciplineWriteOnlyRepository,
+        IUserDisciplineReadOnlyRepository
+    {
+        private readonly QuizzandoDbContext _dbContext;
+
+        public UserDisciplineRepository(
+            QuizzandoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add(UserDiscipline userDiscipline)
+        {
+            await _dbContext.UserDiscipline
+                .AddAsync(userDiscipline);
+        }
+
+        public async Task<bool> ExistsWithUserIdAndDisciplineId(Guid userId, Guid disciplineId)
+        {
+            return await _dbContext.UserDiscipline
+                .AnyAsync(user => user.UserId == userId && user.DisciplineId == disciplineId);
+        }
+    }
+}
