ac5a6d0c9bdec2ef9c55be9522cf28bb14009e99
toffettl <toffetti0824@gmail.com>
2025-09-18 07:57:28 -0300

    V1.0.14

--- Quizzando/Program.cs
+++ Quizzando/Program.cs
@@ -1,15 +1,19 @@
 using DotNetEnv;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Quizzando;
 using Quizzando.AutoMapper;
 using Quizzando.DataAccess;
 using Quizzando.Exception.Filters;
 using Quizzando.Settings;
+using System.Text;
 
 Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration["ConnectionStrings:DefaultConnection"] = Environment.GetEnvironmentVariable("DEFAULT_CONNECTION");
+builder.Configuration["ConnectionStrings:DefaultConnection"] =
+    Environment.GetEnvironmentVariable("DEFAULT_CONNECTION");
 
 builder.Services.AddControllers();
 
@@ -35,6 +39,29 @@ builder.Services.AddCors(options =>
     });
 });
 
+// ? Configura Autenticação com JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -47,6 +74,7 @@ app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
