5decc4ad742861a7c5e1dc7f5cf55af6bb95d65e
aluno <etecae023@hotmail.com>
2025-09-19 11:56:08 -0300

    feat: add Delete function to UserDisciplineRelationController

---
+++ Quizzando/Controllers/UserDisciplineRelationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quizzando.Communication.Requests.UserDiscipline;
+using Quizzando.Communication.Responses;
+using Quizzando.Communication.Responses.UserDiscipline;
+using Quizzando.UseCases.UserDisciplines.Create;
+using Quizzando.UseCases.UserDisciplines.GetByUserIdAndDisciplineId;
+
+namespace Quizzando.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDisciplineRelationController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(UserDisciplineRelationResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Create(
+            [FromServices] ICreateUserDisciplineUseCaseRelation useCase,
+            [FromBody] UserDisciplineRelationRequest request)
+        {
+            var response = await useCase.Execute(request);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete (
+            [FromServices] IDeleteUserDisciplineRelationUseCase useCase,
+            [FromRoute] Guid id)
+        {
+            await useCase.Execute(id);
+
+            return NoContent();
+        }
+    }
+}
