2a0225fccc31608f3a1a969d2a36478d6c1203b7
aluno <etecae023@hotmail.com>
2025-09-22 08:34:04 -0300

    feat: add IGetUserDisciplineRelationsByUserIdUseCase interface

---
+++ Quizzando/UseCases/UserDisciplineRelations/GetByUserId/IGetUserDiciplineRelationsByUserIdUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Quizzando.Communication.Responses.UserDisciplineRelation;
+using Quizzando.Models;
+
+namespace Quizzando.UseCases.UserDisciplineRelations.GetByUserId
+{
+    public interface IGetUserDiciplineRelationsByUserIdUseCase
+    {
+        Task<UserDisciplineRelationResponses> Execute(Guid userId);
+    }
+}
