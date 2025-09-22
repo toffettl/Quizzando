faae14d8279c3bd85958958a60c3bfb0f237171a
aluno <etecae023@hotmail.com>
2025-09-22 08:33:40 -0300

    feat: add UserDisciplineRelationResponses

---
+++ Quizzando/Communication/Responses/UserDisciplineRelation/UserDisciplineRelationResponses.cs
@@ -0,0 +1,9 @@
+﻿using Quizzando.Communication.Responses.UserDiscipline;
+
+namespace Quizzando.Communication.Responses.UserDisciplineRelation
+{
+    public class UserDisciplineRelationResponses
+    {
+        public List<UserDisciplineRelationResponse>? UserDisciplineRelations { get; set; }
+    }
+}
