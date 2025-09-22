0a54a15e7037bf9edc474a3e0f6721042bfc8df3
aluno <etecae023@hotmail.com>
2025-09-22 08:34:22 -0300

    feat: GetUserDIsciplineRelationsByUserIdUseCase

---
+++ Quizzando/UseCases/UserDisciplineRelations/GetByUserId/GetUserDisciplineRelationsByUserIdUseCase.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Quizzando.Communication.Responses.UserDiscipline;
+using Quizzando.Communication.Responses.UserDisciplineRelation;
+using Quizzando.DataAccess.Repositories.UserDisciplineRepositories;
+using Quizzando.Models;
+
+namespace Quizzando.UseCases.UserDisciplineRelations.GetByUserId
+{
+    public class GetUserDisciplineRelationsByUserIdUseCase : IGetUserDiciplineRelationsByUserIdUseCase
+    {
+        private readonly IUserDisciplineRelationReadOnlyRepository _userDisciplineRelationReadOnlyRepository;
+        private readonly IMapper _mapper;
+
+        public GetUserDisciplineRelationsByUserIdUseCase(
+            IUserDisciplineRelationReadOnlyRepository userDisciplineRelationReadOnlyRepository,
+            IMapper mapper)
+        {
+            _userDisciplineRelationReadOnlyRepository = userDisciplineRelationReadOnlyRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDisciplineRelationResponses> Execute(Guid userId)
+        {
+            var userDisciplineRelations = await _userDisciplineRelationReadOnlyRepository.GetUserDiciplineRelationsByUserId(userId);
+
+            return new UserDisciplineRelationResponses
+            {
+                UserDisciplineRelations = _mapper.Map<List<UserDisciplineRelationResponse>>(userDisciplineRelations)
+            };
+        }
+    }
+}
