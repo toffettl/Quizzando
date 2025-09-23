f78bc65482a15f9f2db32e606400ba0683946d77
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-09-22 21:03:58 -0300

    feat: add GetByDisciplineId function to UserDisciplineRelationController

--- Quizzando/Controllers/UserDisciplineRelationController.cs
+++ Quizzando/Controllers/UserDisciplineRelationController.cs
@@ -3,6 +3,7 @@ using Quizzando.Communication.Requests.UserDiscipline;
 using Quizzando.Communication.Responses;
 using Quizzando.Communication.Responses.UserDiscipline;
 using Quizzando.Communication.Responses.UserDisciplineRelation;
+using Quizzando.UseCases.UserDisciplineRelations.GetByDisciplineId;
 using Quizzando.UseCases.UserDisciplineRelations.GetByUserId;
 using Quizzando.UseCases.UserDisciplines.Create;
 using Quizzando.UseCases.UserDisciplines.GetByUserIdAndDisciplineId;
@@ -39,6 +40,21 @@ namespace Quizzando.Controllers
                 return NoContent();
             }
 
+            return Ok(response);
+        }
+
+        [HttpGet("Discipline/{disciplineId}")]
+        [ProducesResponseType(typeof(UserDisciplineRelationResponses), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> GetByDisciplineId(
+            [FromServices] IGetUserDisciplineRelationsByDisciplineIdUseCase useCase,
+            [FromRoute] Guid disciplineId)
+        {
+            var response = await useCase.Execute(disciplineId);
+            if (response.UserDisciplineRelations!.Count == 0)
+            {
+                return NoContent();
+            }
             return Ok(response);
         }
 
