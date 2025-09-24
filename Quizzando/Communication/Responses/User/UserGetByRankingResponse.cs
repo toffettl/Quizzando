9e97608787189261cbd015b8f76e040160c8540b
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-09-24 04:28:08 -0300

    feat: add UserGetByRankingResponse

---
+++ Quizzando/Communication/Responses/User/UserGetByRankingResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Quizzando.Communication.Responses.User
+{
+    public class UserGetByRankingResponse
+    {
+        public Guid Id { get; set; }
+        public string? Username { get; set; }
+        public int Score { get; set; }
+
+    }
+}
