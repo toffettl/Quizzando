30136860d9c4cd712234eacbe6c935e94796f5e0
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-09-24 04:46:34 -0300

    feat: add IGetUsersByRankingUseCase interface

---
+++ Quizzando/UseCases/Users/GetByRanking/IGetUsersByRankingUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Quizzando.Communication.Responses;
+using Quizzando.Communication.Responses.User;
+
+namespace Quizzando.UseCases.Users.GetByRanking
+{
+    public interface IGetUsersByRankingUseCase
+    {
+        Task<PagedResult<UserGetByRankingResponse>> Execute(int page, int pageSize);
+    }
+}
