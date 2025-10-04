aae6fb39dda92346f6314165f1c103a5704ab163
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-10-04 20:59:22 -0300

    feat: add variables in CreateCourseRequest

--- Quizzando/Communication/Requests/Course/CreateCourseRequest.cs
+++ Quizzando/Communication/Requests/Course/CreateCourseRequest.cs
@@ -1,8 +1,14 @@
+using Quizzando.Enums;
+
 namespace Quizzando.Communication.Requests.Course
 {
     public class CreateCourseRequest
     {
-        public string? courseName { get; set; }
-        public Guid DisciplineId { get; set; }
+        public string? CourseName { get; set; }
+        public string? Description { get; set; }
+        public string? BackgroundImage { get; set; }
+        public string? Icon { get; set; }
+        public CourseCategory Category { get; set; }
+        public int Rating { get; set; }
     }
 }
